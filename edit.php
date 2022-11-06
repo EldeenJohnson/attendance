bbe32720cd593a19e4365cc015478c9e1cf2b528
EldeenJohnson <77896084+EldeenJohnson@users.noreply.github.com>
2022-11-06 11:12:26 -0500

    update

--- edit.php
+++ edit.php
@@ -54,9 +54,10 @@
             <input type="email" class="form-control" value="<?php echo $attendee['emailaddress'] ?>" id="email" name="email" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
-<div class="d-grid gap-2">
+    <div >      
+            <button type="submit" name="submit" class="btn btn-outline-secondary">Return to List</button>
             <button type="submit" name="submit" class="btn btn-success">Save</button>
-</div>
+    </div>
     </form>
 <?php } ?>
 <br>
